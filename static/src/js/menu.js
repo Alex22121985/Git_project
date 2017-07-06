4547d78744f1671d958ff27e448924993a8db530
Alex22121985 <rhcp-sk@mail.ru>
2017-07-06 11:11:10 +0500

    Delete menu.js

--- static/src/js/menu.js
+++
@@ -1,9 +0,0 @@
-odoo.define('dropext.support_menu', function (require) {
-"use strict";
-var UserMenu = require('web.UserMenu');
-UserMenu.include({
-    on_menu_support_menu: function () {
-        window.open('https://suvit.ru', '_blank');
-    }
-});
-});
